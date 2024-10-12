55a1cc4534d5353113cd96c5ea52e073cf3d70ad
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-12 19:02:58 +0400

    add database connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
  const express = require('express');
  require('dotenv').config();
  const cookieParser = require('cookie-parser');
+const { connectDB } = require('./config/db');
 
  
  const app = express();
@@ -9,6 +10,9 @@ app.use(cookieParser());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
+
 
 app.listen(PORT, () => console.log('server is running'));
 
