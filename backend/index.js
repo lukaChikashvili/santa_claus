f54a1939e313cfe38ed3ed2ab0fec69ddeeb80ba
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-12 19:10:48 +0400

    add mongoose model

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
  require('dotenv').config();
  const cookieParser = require('cookie-parser');
 const { connectDB } = require('./config/db');
+const routes = require('./routes/userRoutes')
 
  
  const app = express();
@@ -11,7 +12,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
+app.use('/api', routes );
+
 connectDB();
+
 
 
 app.listen(PORT, () => console.log('server is running'));
