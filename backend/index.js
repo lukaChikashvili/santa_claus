062a72f116eab2f3f20cb448fdaaf2ed5f9a8432
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-12 16:24:23 +0400

    add backend server

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
+ const express = require('express');
+ require('dotenv').config();
+ const cookieParser = require('cookie-parser');
+
  
+ const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log('server is running'));
+
