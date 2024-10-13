97ddbd0102110de15219f4e770c47cc7e1421f25
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-13 17:07:44 +0400

    add shaders and fragment and vertex

--- frontend/anonim/src/components/Experience.jsx
+++ frontend/anonim/src/components/Experience.jsx
@@ -1,15 +1,25 @@
 import { OrbitControls } from '@react-three/drei'
 import React from 'react'
+import vertexShader from '../shaders/vertexShader.glsl';
+import fragmentShader from '../shaders/fragmentShader.glsl';
 
 const Experience = () => {
+
+
   return (
   <>
         <OrbitControls />
 
-     <mesh >
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
+<mesh>
+  <planeGeometry args={[3, 3, 64, 64]} />
+  <shaderMaterial 
+  wireframe = {true} 
+  vertexShader={vertexShader}
+  fragmentShader={fragmentShader}
+  />
+
+</mesh>
+     
   </>
   )
 }
