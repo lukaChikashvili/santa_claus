1f156f17a11fdcff8bbb8a21ba676e9a8d7b852c
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-13 15:49:25 +0400

    add register design

--- frontend/anonim/src/components/NavBar.jsx
+++ frontend/anonim/src/components/NavBar.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { KeyRound, UserPlus } from 'lucide-react'
+import { KeyRound, MenuIcon, UserPlus } from 'lucide-react'
 
 const NavBar = () => {
   return (
     <header className='w-full  flex items-center justify-between px-16 py-8'>
-       <h1 className='text-white text-2xl font-bold'>მომიყევი+</h1>
+       <h1 className='text-white text-2xl font-bold hidden md:block' >მომიყევი+</h1>
 
-       <nav className='flex items-center gap-8 text-white font-bold text-xl' >
+       <nav className='hidden md:flex items-center gap-8 text-white font-bold text-xl ' >
 
           <Link to = "/register" className='flex items-center gap-4'><UserPlus />რეგისტრაცია</Link>
           <Link to = "/login" className='flex items-center gap-4'><KeyRound />შესვლა</Link>
+         
        </nav>
+
+       
     </header>
   )
 }
--- frontend/anonim/src/components/Register.jsx
+++ frontend/anonim/src/components/Register.jsx
@@ -37,15 +37,24 @@ const Register = () => {
     }
 
   return (
-    <div>
-        <form onSubmit={handleRegister}>
-       <input type='text' value={username} placeholder='username...' onChange={(e) => setUsername(e.target.value)} />
-       <input type='email' value ={email} placeholder='email..' onChange={(e) => setEmail(e.target.value)} />
-       <input type='password' value = {password} placeholder='password..' onChange={(e) => setPassword(e.target.value)}  />
+    <div className='w-full flex items-center justify-between px-36 mt-[5rem]'>
 
-       <button type='submit'>{isLoading ? "registering.." : "register"}</button>
+      <div>
+srtsrt
+
+      </div>
+
+<div className='w-[35%] flex flex-col gap-8 '>
+      <h1 className='text-4xl text-white font-bold'>რეგისტრაცია</h1>
+      <span className='w-full h-[1.4px] bg-[#BED754]'></span>
+        <form onSubmit={handleRegister} className='flex flex-col gap-8 '>
+       <input type='text' value={username} placeholder='სახელი...' onChange={(e) => setUsername(e.target.value)} className='p-2 rounded outline-none shadow'/>
+       <input type='email' value ={email} placeholder='ელ-ფოსტა..' onChange={(e) => setEmail(e.target.value)} className='p-2 rounded outline-none shadow' />
+       <input type='password' value = {password} placeholder='პაროლი..' onChange={(e) => setPassword(e.target.value)} className='p-2 rounded outline-none shadow' />
+
+       <button type='submit' className='w-full bg-[#BED754] rounded shadow-lg text-white  shadow-gray-700 p-2 font-semibold duration-500 ease hover:bg-[#6b7a26]'>{isLoading ? "იტვირთება.." : "რეგისტრაცია"}</button>
        </form>
-
+       </div>
     </div>
   )
 }
