a5f57593b914b202d1d213ff54288d38305203bd
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-27 15:43:13 +0400

    add white plane reflective

--- frontend/anonim/src/components/Experience.jsx
+++ frontend/anonim/src/components/Experience.jsx
@@ -1,48 +1,26 @@
-import { OrbitControls, Stars, useTexture } from '@react-three/drei'
-import React, { useRef } from 'react'
-import vertexShader from '../shaders/vertexShader.glsl';
-import fragmentShader from '../shaders/fragmentShader.glsl';
-import * as THREE from 'three'
-import { useFrame } from '@react-three/fiber';
-import paperImg from '../assets/paper.jpg';
-
-
+import { OrbitControls, Reflector } from '@react-three/drei'
 const Experience = () => {
 
-    // load paper texture
-    const paper = useTexture(paperImg);
-
-
-  const uniforms = useRef({
-    uTime: { value: 0},
-    uHeight: { value: 0.4 },
-    uFrequency: { value: new THREE.Vector2(2, 2)},
-    uTexture: {value: paper}
-  });
-
-
-
-  useFrame(() => {
-      uniforms.current.uTime.value += 0.04;
-  })
 
   return (
   <>
-
-
-
-    <Stars count={2000}/>
-
-<mesh position = {[0, -0.5, 0]} rotation={[0, 0, 0]}  >
-  <planeGeometry args={[7, 6, 64, 64]} />
-  <shaderMaterial 
-  vertexShader={vertexShader}
-  fragmentShader={fragmentShader}
-  uniforms={uniforms.current}
-  />
-
-</mesh>
-     
+     <OrbitControls />
+      <Reflector 
+       blur={[400, 100]}
+       resolution={1024}
+       mixBlur={1}
+       mixStrength={1}
+       depthScale={1}
+       minDepthThreshold={0.8}
+       maxDepthThreshold={1}
+       rotation = {[-Math.PI / 2, 0, 0]}
+       args={[40, 30]}
+       position={[0, -2, 4]}
+       
+       
+       >
+       {(Material, props) => <meshStandardMaterial {...props} color="#F5F5F5" roughness={0.3} metalness={0.6} />}
+      </Reflector>
   </>
   )
 }
