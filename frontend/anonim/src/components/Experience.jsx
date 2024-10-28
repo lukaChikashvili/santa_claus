d5eb565337c77a9907893d7ede4a86d26e0b9e48
luka chikashvili <lukachikashvili2020@gmail.com>
2024-10-28 17:20:32 +0400

    add sofa and santa image

--- frontend/anonim/src/components/Experience.jsx
+++ frontend/anonim/src/components/Experience.jsx
@@ -1,80 +1,95 @@
-import { OrbitControls,  useGLTF,  useTexture } from '@react-three/drei'
-import wood from '../assets/wood.jpg'
-import { Bloom, EffectComposer, Vignette } from '@react-three/postprocessing'
+import { OrbitControls, Text3D, useGLTF, useTexture } from '@react-three/drei';
+import wood from '../assets/wood.jpg';
+import { Bloom, EffectComposer } from '@react-three/postprocessing';
+import { Physics, RigidBody } from '@react-three/rapier';
+import gift from '../assets/gift.jpg';
+import giftblue from '../assets/giftblue.jpg'
+import santa from '../assets/santa.jpg'
+
 
 const Experience = () => {
+  // Add texture
+  const woodTexture = useTexture(wood);
+  const giftTexture = useTexture(gift);
+  const giftblueTexture = useTexture(giftblue);
+  const santaTexture = useTexture(santa);
+  
+  // Load tree 3D model
+  const treeModel = useGLTF('./tree.glb');
+  const sofaModel = useGLTF('./sofa.glb');
 
-     // add texture
-    const woodTexture = useTexture(wood);
-
-
-    // tree 3d model
-    const treeModel = useGLTF('./tree.glb');
-
-// add bloom effect
+  return (
+    <>
+      <OrbitControls makeDefault minAzimuthAngle={-Math.PI / 4} maxAzimuthAngle={Math.PI / 4} />
+      
+      <Physics>
+     
+        <RigidBody type='fixed'>
+          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
+            <planeGeometry args={[40, 40]} />
+            <meshStandardMaterial map={woodTexture} />
+          </mesh>
+        </RigidBody>
 
    
-  return (
-  <>
-     <OrbitControls makeDefault minAzimuthAngle={-Math.PI / 4} maxAzimuthAngle={Math.PI / 4} />
+        {[-20, -10, 0, 10, 20].map((pos) => (
+          <mesh key={pos} rotation={[-Math.PI / 2, 0, 0]} castShadow position={[pos, 15, 0]}>
+            <boxGeometry args={[5, 40]} />
+            <meshStandardMaterial color={[5, 2, 1]} />
+          </mesh>
+        ))}
 
+       
+        <RigidBody mass={1} restitution={0.7} friction={0.5} colliders="cuboid">
+          <mesh  position={[0, 20, 0]}>
+            <boxGeometry args={[4, 4, 4]} />
+            <meshStandardMaterial map={giftTexture}/>
+          </mesh>
+        </RigidBody>
 
+        <RigidBody mass={1} restitution={0.7} friction={0.5} colliders="cuboid">
+          <mesh  position={[4, 30, 2]}>
+            <boxGeometry args={[4, 4, 4]} />
+            <meshStandardMaterial map={giftblueTexture}/>
+          </mesh>
+        </RigidBody>
 
+       
 
-       <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} >
-         <planeGeometry args={[40, 40]}/>
-         <meshStandardMaterial map={woodTexture} />
-       </mesh>
+<RigidBody type='fixed'>
+        <mesh position={[0, 10, -15]} rotation={[0, 0, 0]}>
+          <planeGeometry args={[40, 20]} />
+          <meshStandardMaterial map={woodTexture} roughness={0.8} metalness={0.5} />
+        </mesh>
+        </RigidBody>
 
-       <mesh rotation={[-Math.PI / 2, 0, 0]} castShadow position={[0, 15, 0]}>
-        <boxGeometry args={[5, 40]} />
-        <meshStandardMaterial color={[5, 2, 1]} />
-       </mesh>
+        <RigidBody type='fixed'>
+        <mesh position={[-20, 10, 0]} rotation={[0, Math.PI / 2, 0]}>
+          <planeGeometry args={[30, 20]} />
+          <meshStandardMaterial map={woodTexture} roughness={0.8} metalness={0.5} />
+        </mesh>
+        </RigidBody>
 
-       <mesh rotation={[-Math.PI / 2, 0, 0]} castShadow position={[10, 15, 0]}>
-        <boxGeometry args={[5, 40]} />
-        <meshStandardMaterial color={[5, 2, 1]} />
-       </mesh>
+        <RigidBody type='fixed'>
+        <mesh position={[20, 10, 0]} rotation={[0, -Math.PI / 2, 0]}>
+          <planeGeometry args={[30, 20]} />
+          <meshStandardMaterial map={woodTexture} roughness={0.8} metalness={0.5} />
+        </mesh>
+        </RigidBody>
 
-       <mesh rotation={[-Math.PI / 2, 0, 0]} castShadow position={[-10, 15, 0]}>
-        <boxGeometry args={[5, 40]} />
-        <meshStandardMaterial color={[5, 2, 1]} />
-       </mesh>
+        <mesh position={[-15, 8, 12]} rotation={[0, Math.PI / 2, 0]}>
+          <planeGeometry args={[3, 4]} />
+          <meshStandardMaterial map={santaTexture} />
+        </mesh>
+        
 
-       <mesh rotation={[-Math.PI / 2, 0, 0]} castShadow position={[-20, 15, 0]}>
-        <boxGeometry args={[5, 40]} />
-        <meshStandardMaterial color={[5, 2, 1]} />
-       </mesh>
+     
+        <primitive object={treeModel.scene} scale={0.3} position={[-9, 0, 10]} />
+        <primitive object={sofaModel.scene} scale={7} position={[-9, 0, -8]} />
+        </Physics>
+ 
+    </>
+  );
+};
 
-       <mesh rotation={[-Math.PI / 2, 0, 0]} castShadow position={[20, 15, 0]}>
-        <boxGeometry args={[5, 40]} />
-        <meshStandardMaterial color={[5, 2, 1]} />
-       </mesh>
-
-
-
-
-      <mesh position={[0, 10, -15]} rotation={[0, 0, 0]}>
-        <planeGeometry args={[40, 20]} />
-        <meshStandardMaterial map={woodTexture} roughness={0.8} metalness={0.5} />
-      </mesh>
-
-
-      <mesh position={[-20, 10, 0]} rotation={[0, Math.PI / 2, 0]}>
-        <planeGeometry args={[30, 20]} />
-        <meshStandardMaterial map={woodTexture} roughness={0.8} metalness={0.5} />
-      </mesh>
-
-
-      <mesh position={[20, 10, 0]} rotation={[0, -Math.PI / 2, 0]}>
-        <planeGeometry args={[30, 20]} />
-        <meshStandardMaterial map={woodTexture} roughness={0.8} metalness={0.5} />
-      </mesh>
-
-      <primitive object={treeModel.scene} scale = {0.30} position = {[-9, 0, 10]} />
-  </>
-
-  )
-}
-
-export default Experience
+export default Experience;
